93441433f2b7f1c777c3f124624b406c336e5a9e
alexhub8 <aalexander.email@protonmail.com>
2026-02-27 18:42:52 -0300

    new file:   README.MD 	new file:   package.json 	new file:   src/app.js 	new file:   src/data/Products.json 	new file:   src/data/carts.json 	new file:   src/managers/CartManager.js 	new file:   src/managers/ProductManager.js 	new file:   src/routes/Products.Routes.js 	new file:   src/routes/carts.router.js

---
+++ src/routes/Products.Routes.js
@@ -0,0 +1,42 @@
+import { Router } from "express";
+import ProductManager from "../managers/ProductManager.js";
+
+const router = Router();
+const manager = new ProductManager("./src/data/products.json");
+
+router.get("/", async (req, res) => {
+  const products = await manager.getProducts();
+  res.json(products);
+});
+
+router.get("/:pid", async (req, res) => {
+  const product = await manager.getProductById(req.params.pid);
+  product
+    ? res.json(product)
+    : res.status(404).json({ error: "Producto no encontrado" });
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const newProduct = await manager.addProduct(req.body);
+    res.status(201).json(newProduct);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.put("/:pid", async (req, res) => {
+  const updated = await manager.updateProduct(req.params.pid, req.body);
+  updated
+    ? res.json(updated)
+    : res.status(404).json({ error: "Producto no encontrado" });
+});
+
+router.delete("/:pid", async (req, res) => {
+  const deleted = await manager.deleteProduct(req.params.pid);
+  deleted
+    ? res.json({ message: "Producto eliminado", product: deleted })
+    : res.status(404).json({ error: "Producto no encontrado" });
+});
+
+export default router;
